7e467615cbef4444425e9419d9bf8af29453525f
Jing <jhu8187@gmail.com>
2023-08-20 14:33:18 -0400

    feat: update user and finance data structure

--- lib/models/finance_item.dart
+++ lib/models/finance_item.dart
@@ -1,5 +1,7 @@
 // Properties of a finance item in a finance list
 class FinanceItem {
+  final List<String> participants;
+
   // date of transaction
   final int dateMonth;
   final int dateDay;
@@ -7,9 +9,9 @@ class FinanceItem {
   final String desc;
   final double amount;
 
-  // a percentage or amount or no split. (AA, 20% 80%, joint account)
-  final double split;
-  final String tempPay;
+  // AA, %, exact amount, no split
+  final String splitOption;
+  final bool isTempPay;
 
   // type of transaction (income or expense)
   final String type;
@@ -18,20 +20,22 @@ class FinanceItem {
   final String currency;
 
   FinanceItem({
+    required this.participants,
     required this.dateMonth,
     required this.dateDay,
     required this.desc,
     required this.amount,
-    required this.split,
-    required this.tempPay,
     required this.type,
     required this.category,
     required this.currency,
+    required this.splitOption,
+    this.isTempPay = false,
   });
 
   // convert data for database
   toJson() {
     return {
+      "participants": participants,
       "dateMonth": dateMonth,
       "dateDay": dateDay,
       "type": type,
@@ -39,8 +43,54 @@ class FinanceItem {
       "desc": desc,
       "currency": currency,
       "amount": amount,
-      "split": split,
-      "tempPay": tempPay,
+      "splitOption": splitOption,
+      "isTempPay": isTempPay,
     };
+  }
+
+  // get number of participated users
+  int getParticipants() {
+    return participants.length;
+  }
+
+  // get participated user
+  String getParticipant(String user) {
+    return participants[int.parse(user)];
+  }
+
+  // set/change current participated user
+  String setParticipant(String user, String newUser) {
+    return participants[int.parse(user)] = newUser;
+  }
+
+  // add participated user
+  void addParticipant(String newUser) {
+    for (var user in participants) {
+      if (newUser != user) {
+        participants.add(newUser);
+      }
+    }
+  }
+
+  // get weekday from a dateTime object
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thu';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+    }
   }
 }
---
+++ lib/models/user.dart
@@ -0,0 +1,27 @@
+// Properties of a registered user
+
+class User {
+  final String name;
+  Map<String, double>
+      tempPayment; // <user docId, amount paid> e.g p1 owes you xxx
+  Map<String, double>
+      splitPayment; // <user docId, amount owed> e.g. You owe p2 xxx
+
+  User({
+    required this.name,
+    this.tempPayment = const {},
+    this.splitPayment = const {},
+  });
+
+  // get total temp payment
+  double getTotalTemp() {
+    var values = tempPayment.values;
+    return values.fold(0.0, (sum, value) => sum + value);
+  }
+
+  // get total split payment
+  double getTotalSplit() {
+    var values = splitPayment.values;
+    return values.fold(0.0, (sum, value) => sum + value);
+  }
+}
