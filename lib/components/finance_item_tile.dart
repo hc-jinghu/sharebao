99e51aec025b36ee095a81ed8eaab29fda771bf7
Jing <jhu8187@gmail.com>
2023-08-24 17:34:56 -0400

    feat: add date year

--- lib/components/finance_item_tile.dart
+++ lib/components/finance_item_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sharebao/pages/finance_item_page.dart';
 
 class FinanceListTile extends StatelessWidget {
   final Map<String, dynamic> item;
@@ -6,41 +7,51 @@ class FinanceListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      shape: Border(
-        bottom: BorderSide(
-          color: Color.fromARGB(255, 209, 209, 209),
-          width: 0.8,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => FinanceItemPage(item: item),
+          ),
+        );
+      },
+      child: ListTile(
+        shape: const Border(
+          bottom: BorderSide(
+            color: Color.fromARGB(255, 209, 209, 209),
+            width: 0.8,
+          ),
         ),
-      ),
-      // tileColor: Colors.pink,
-      title: Text(item['desc']),
-      subtitle: Text('${item['dateMonth']}/${item['dateDay']}'),
-      leading: Wrap(
-        direction: Axis.horizontal,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        spacing: 5,
-        children: [
-          SizedBox(
-            height: 30,
-            width: 30,
-            child: Image.asset('lib/icons/bill.png'),
-          ),
-          Wrap(
-            direction: Axis.vertical,
-            children: [
-              Text(
-                item['category'],
-                style: TextStyle(fontSize: 16),
-              ),
-              Text(item['split'].toString())
-            ],
-          ),
-        ],
-      ),
-      trailing: Text(
-        '\$${item['amount']}',
-        style: TextStyle(fontSize: 18),
+        // tileColor: Colors.pink,
+        title: Text(item['desc']),
+        subtitle: Text('${item['dateMonth']}/${item['dateDay']}'),
+        leading: Wrap(
+          direction: Axis.horizontal,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          spacing: 5,
+          children: [
+            SizedBox(
+              height: 30,
+              width: 30,
+              child: Image.asset('lib/icons/bill.png'),
+            ),
+            Wrap(
+              direction: Axis.vertical,
+              children: [
+                Text(
+                  item['category'],
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(item['split'].toString())
+              ],
+            ),
+          ],
+        ),
+        trailing: Text(
+          '\$${item['amount']}',
+          style: TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
