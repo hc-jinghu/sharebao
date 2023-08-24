7316f951d2b4d24d47f5d301e42ffea9cfb8ef8e
Jing <jhu8187@gmail.com>
2023-08-24 17:36:14 -0400

    file update for previous commit

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+var db = FirebaseFirestore.instance;
 
 // default background color for the app
 var backgroundColor = Colors.grey[300];
+
+// Border color for Split option
+var borderColor = Colors.grey;
+
+// split options
+enum SplitOptions { equal, percent, exact, none }
+
+SplitOptions selectedSplit = SplitOptions.equal;
+
+// months
+String monthEng(int month) {
+  switch (month) {
+    case 1:
+      return "January";
+    case 2:
+      return "February";
+    case 3:
+      return "March";
+    case 4:
+      return "April";
+    case 5:
+      return "May";
+    case 6:
+      return "June";
+    case 7:
+      return "July";
+    case 8:
+      return "August";
+    case 9:
+      return "September";
+    case 10:
+      return "October";
+    case 11:
+      return "November";
+    case 12:
+      return "December";
+    default:
+      return "";
+  }
+}
